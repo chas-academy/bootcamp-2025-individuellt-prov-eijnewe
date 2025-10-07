04348c4b9fbd02096e98b845ad5fca9d73d3a057
Wenjie <wenjie.egerbladh@gmail.com>
2025-10-07 10:59:21 +0200

    done

--- q9.js
+++ q9.js
@@ -1,20 +1,32 @@
 // skapa funktionerna som anropas i myCalculator så att de funkar så som koden och alla "cases" antyder
 
 export function myCalculator(operator, num1, num2) {
-  switch (operator) {
-    case "+":
-      return myAddFunction(num1, num2);
+    switch (operator) {
+        case '+':
+            function myAddFunction(num1, num2) {
+                return num1 + num2
+            }
+            return myAddFunction(num1, num2)
 
-    case "-":
-      return mySubtractFunction(num1, num2);
+        case '-':
+            function mySubtractFunction(num1, num2) {
+                return num1 - num2
+            }
+            return mySubtractFunction(num1, num2)
 
-    case "*":
-      return myMultiplyFunction(num1, num2);
+        case '*':
+            function myMultiplyFunction(num1, num2) {
+                return num1 * num2
+            }
+            return myMultiplyFunction(num1, num2)
 
-    case "/":
-      return myDivisionFunction(num1, num2);
+        case '/':
+            function myDivisionFunction(num1, num2) {
+                return num1 / num2
+            }
+            return myDivisionFunction(num1, num2)
 
-    default:
-      return "Error operator not found!";
-  }
+        default:
+            return 'Error operator not found!'
+    }
 }
